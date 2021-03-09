bedaed44364b67a19847d86898e9209b1233bdcb
TNMayer <thomas.mayer@smail.auswaertiges-amt.de>
2021-03-09 12:16:03 +0100

    adapted nameconventions

--- js/app.js
+++ js/app.js
@@ -18,8 +18,8 @@
  * 
 */
 
-const sections = document.querySelectorAll('*[id^="section"]');
-const navigationBar = document.querySelector("#navbar__list");
+const pageBlocks = document.querySelectorAll('*[id^="section"]');
+const navigationBarList = document.querySelector("#navbar__list");
 
 /**
  * End Global Variables
@@ -28,37 +28,37 @@ const navigationBar = document.querySelector("#navbar__list");
 */
 
 /* function adapted from https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/#:~:text=Demo-,Summary,in%20the%20viewport%20or%20not */
-function elementInViewport (elem) {
+function elementVisible (elem) {
     /*
     function identivies if the given output is visible in viewport
     it returns a boolean 
     */
-    const boundingBox = elem.getBoundingClientRect();
+    const rectangle = elem.getBoundingClientRect();
     return (
-        boundingBox.top >= -5 &&
-        boundingBox.top <= 500 &&
-        boundingBox.left >= 0
+        rectangle.top >= -5 &&
+        rectangle.top <= 500 &&
+        rectangle.left >= 0
     );
 };
 
-function addNavActiveState(section) {
+function addNavActiveState(pageBlock) {
     /*
     based on the given section the function adds the class active to the navigation bar
     no return value
     */
-    const id = section.id;
-    const navElem = document.querySelector('[data-id="' + id + '"]')
+    const id = pageBlock.id;
+    const navElem = document.querySelector('[data-identifier="' + id + '"]')
     
     navElem.classList.add("active");
 }
 
-function removeNavActiveState(section) {
+function removeNavActiveState(pageBlock) {
     /*
     based on the given section the function removes the class active to the navigation bar
     no return value
     */
-    const id = section.id;
-    const navElem = document.querySelector('[data-id="' + id + '"]')
+    const id = pageBlock.id;
+    const navElem = document.querySelector('[data-identifier="' + id + '"]')
     
     navElem.classList.remove("active");
 }
@@ -70,53 +70,53 @@ function removeNavActiveState(section) {
 */
 
 // build the nav
-function buildNavigation() {
+function createNavBar() {
     /*
     the function identifies all sections present in index.html and builds a navigation menu based on those sections
     */
-    for (section of sections) {
-        const navName = section.getAttribute('data-nav');
+    for (pageBlock of pageBlocks) {
+        const navName = pageBlock.getAttribute('data-nav');
         const navElement = document.createElement("li");
         navElement.classList.add("menu__link");
         navElement.innerText = navName;
         navElement.setAttribute("title", "Scroll to " + navName);
-        navElement.setAttribute("data-id", section.id);
-        if (section.classList.contains("your-active-class")) {
+        navElement.setAttribute("data-identifier", pageBlock.id);
+        if (pageBlock.classList.contains("your-active-class")) {
             navElement.classList.add("active");
         }
-        navigationBar.appendChild(navElement);
+        navigationBarList.appendChild(navElement);
     }
 }
 
 // Add class 'active' to section when near top of viewport
 
-function setSectionActive() {
+function setPageBlockActive() {
     /*
     function iterates over all sections in index.html, identifies if the given section is in viewport and adds active css to the navigation bar and the section itself
     no return value
     */
-    for(section of sections) {
-        if(elementInViewport(section)) {
-            section.classList.add("your-active-class");
-            addNavActiveState(section);
+    for(pageBlock of pageBlocks) {
+        if(elementVisible(pageBlock)) {
+            pageBlock.classList.add("your-active-class");
+            addNavActiveState(pageBlock);
         } else {
-            section.classList.remove("your-active-class");
-            removeNavActiveState(section);
+            pageBlock.classList.remove("your-active-class");
+            removeNavActiveState(pageBlock);
         }
     }
 }
 
 // Scroll to anchor ID using scrollTO event
 
-function scrollToElement (evt) {
+function scrollPageBlock (evt) {
     /*
     if a section is clicked in the navigationbar the function scrolls the viewport to the clicked section
     */
     if((evt.target.nodeName === 'LI')) {
-        const sectionId = evt.target.getAttribute("data-id");
-        const section = document.getElementById(sectionId);
+        const pageBlockId = evt.target.getAttribute("data-identifier");
+        const pageBlock = document.getElementById(pageBlockId);
 
-        section.scrollIntoView({block: "end", behavior: "smooth"});
+        pageBlock.scrollIntoView({block: "end", behavior: "smooth"});
     }
 };
 
@@ -128,16 +128,16 @@ function scrollToElement (evt) {
 
 // Build menu
 
-buildNavigation();
+createNavBar();
 
 // Scroll to section on link click
 
-navigationBar.addEventListener('click', function(event) {
-    scrollToElement(event);
+navigationBarList.addEventListener('click', function(event) {
+    scrollPageBlock(event);
 });
 
 // Set sections as active
 
 document.addEventListener('scroll', function () {
-    setSectionActive()
+    setPageBlockActive()
 });
